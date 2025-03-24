86a73fba215131f42084b35c072a9e1228bca23c
goncaloafmatos <goncaloafmatos@tecnico.ulisboa.pt>
2025-03-24 14:23:48 +0000

    index.html restructured to isolate js and css into separate files

---
+++ js/main.js
@@ -0,0 +1,323 @@
+// Constants
+const LAYER_SHADOW = "layer-shadow";
+const LAYER_SUN = "layer-sun";
+
+// Parameters
+const TILE_SIZE = 512;
+let CURRENT_LAYER = LAYER_SHADOW;
+
+// DOM elements
+const loader = document.getElementById('loader');
+const dialog_keys = document.getElementById('modal-keys');
+const div_panel = document.getElementById('panel');
+const dialog_help = document.getElementById('modal-help');
+const dialog_about = document.getElementById('modal-about');
+const dialog_success = document.getElementById('modal-success');
+const dialog_error = document.getElementById('modal-error');
+
+// Export handling
+async function generateExport(map, shademapKey) {
+
+    // Compute parameters
+    let zoom = parseFloat(document.getElementById("zoom").value.trim());
+    let nw = [parseFloat(document.getElementById("nw-lat").value.trim()), parseFloat(document.getElementById("nw-lon").value.trim())];
+    let se = [parseFloat(document.getElementById("se-lat").value.trim()), parseFloat(document.getElementById("se-lon").value.trim())];
+    let iterations = parseInt(document.getElementById("iterations").value.trim());
+
+    let dates = document.getElementById("dates").value.split(/,|;| /).map(v => v.trim()).filter(v => !!v);
+    let timestamps = [];
+    if (CURRENT_LAYER === LAYER_SHADOW) {
+        let hours = document.getElementById("hours").value.split(/,|;| /).map(v => v.trim()).filter(v => !!v);
+        if (hours.length && dates.length) {
+            timestamps = dates.map(d => hours.map(h => new Date(`${d}T${h}`))).flat(1);
+        }
+    } else { // LAYER_SUN
+        let hour_start = document.getElementById("hour-start").value.trim();
+        let hour_end = document.getElementById("hour-end").value.trim();
+        if (dates.length && !!hour_start && !!hour_end) {
+            timestamps = dates.map(d => [new Date(`${d}T${hour_start}`), new Date(`${d}T${hour_end}`)])
+        }
+    }
+
+    // Validate if there is any missing
+    if (!zoom || !nw.every(v => !!v) || !se.every(v => !!v) || !timestamps.length || !iterations) {
+        let message = `Invalid parameters!`;
+        dialog_error.getElementsByTagName("p")[0].innerHTML = message;
+        dialog_error.classList.remove("hidden");
+        throw new Error(message);
+    }
+
+    // Show loader and hide modal
+    div_panel.classList.add("hidden");
+    loader.style.display = `block`;
+
+    let shadeMap = shadeMapBuild(map, shademapKey, CURRENT_LAYER).addTo(map);
+    let mLoaded = SMUtils.mapLoaded(map);
+    let smLoaded = shadeMapLoaded(shadeMap);
+    await Promise.all([mLoaded, smLoaded]);
+
+    // Derived parameters
+    const worldSize = TILE_SIZE * 2 ** zoom;
+
+    const { innerWidth, innerHeight } = window;
+    const [xMin, yMin] = unproject(nw, zoom);
+    const [xMax, yMax] = unproject(se, zoom);
+
+    // Area to export is divided into several screens, according to defined parameters
+    const { width, height, wScreens, hScreens, newSE } = roundToScreenSize(innerWidth, innerHeight, nw, se, worldSize);
+    se[0] = newSE.lat;
+    se[1] = newSE.lng;
+
+    console.log("inner width x height", innerWidth, innerHeight);
+    console.log("width x height", width, height);
+    console.log("screens", wScreens, hScreens);
+
+    let screensGlobal = [];
+    for (let i = 0; i < wScreens; i++) {
+        for (let j = 0; j < hScreens; j++) {
+            screensGlobal.push([xMin + i * innerWidth, yMin + j * innerHeight]);
+        }
+    }
+    console.log("screens", screensGlobal);
+
+    // Divide screens array into several batches, making sure each does not exceed MAX_ARRAY_SIZE
+    // AND if changes column, must cover same rows as the previous (to avoid black areas, since the generated TIFF is a square)
+    // Each batch will produce a TIFF file
+    const MAX_ARRAY_SIZE = 2 ** 26;
+    const windowPixels = innerHeight * innerWidth; // Pixels that the browser window covers
+    const areaPixels = width * height; // Pixels covered by the area to export
+    const rows = Math.ceil(height / innerHeight); // Number of sheets rows
+    const cols = Math.ceil(width / innerWidth); // Number of sheets cols
+
+    let screensBatches = [];
+    let batch = [];
+    let batchSize = 0;
+    let colI = 0;
+    let rowI = 0;
+    let colStart = 0;
+    let rowStart = 0;
+    let colEnd = 0;
+    let rowEnd = 0;
+
+    for (let i = 0; i < screensGlobal.length; i++) {
+        // Append to batch
+        batch.push(screensGlobal[i]);
+        batchSize += windowPixels;
+        colEnd = colI;
+        rowEnd = rowI;
+
+        // Update next index
+        rowI += 1;
+        if (rowI >= rows) {
+            rowI = 0;
+            colI += 1;
+        }
+
+        // If last screen 
+        // OR next passes to next column AND (current batch did not start on first column OR array has no space for a full column)
+        // # Last condition avoids black spaces because row or column were not filled totally
+        if (
+            i + 1 == screensGlobal.length ||
+            colI > colStart && (rowStart > 0 || rowI === 0 && (batchSize + windowPixels * rows) > MAX_ARRAY_SIZE)
+        ) {
+            screensBatches.push({ colStart, colEnd, rowStart, rowEnd, screens: batch });
+            batch = [];
+            batchSize = 0;
+
+            colStart = colI;
+            rowStart = rowI;
+        }
+    }
+
+    console.log("Generating", screensBatches.length, "batches, on a canvas with", cols, "cols and", rows, "rows");
+    console.log("screensBatches", screensBatches);
+
+    // For each timestamp, and for every batch, generate TIFF
+    let screenGlobalCounter = 0;
+    for (let ti = 0; ti < timestamps.length; ti++) {
+        let timestamp = timestamps[ti];
+
+        if (CURRENT_LAYER === LAYER_SHADOW) {
+            shadeMap.options.date = timestamp;
+        } else { // LAYER_SUN
+            shadeMap.options.date = timestamp[0];
+            shadeMap.options.sunExposure.startDate = timestamp[0];
+            shadeMap.options.sunExposure.endDate = timestamp[1];
+            shadeMap.options.sunExposure.iterations = iterations;
+        }
+
+        console.log("TIMESTAMP", timestamp, "shademap", shadeMap.options);
+        // TODO! Change timestamp inside shademap
+
+        for (let b = 0; b < screensBatches.length; b++) {
+            let batch = screensBatches[b];
+            let screens = batch['screens'];
+
+            let batchCols = (batch['colEnd'] - batch['colStart'] + 1);
+            let batchRows = batchCols == 1 ? (batch['rowEnd'] - batch['rowStart'] + 1) // If on same column, just compute row difference
+                : rows; // If changes column, should have all rows
+
+            // Proceed with download
+            console.log("Processing batch", b, "with pixels", (width / cols * batchCols) * (height / rows * batchRows));
+            const bitmap = new Uint8Array((width / cols * batchCols) * (height / rows * batchRows));
+            for (let i = 0; i < screens.length; i++) {
+                screenGlobalCounter += 1;
+                console.log("> Processing screen", screenGlobalCounter, "of", screens.length, "(", screensGlobal.length, ")");
+                let progress = (screenGlobalCounter / screensGlobal.length / timestamps.length * 100).toFixed(0);
+                loader.getElementsByTagName("p")[0].innerHTML = `${progress}%`;
+                loader.getElementsByTagName("progress")[0].value = progress;
+                loader.getElementsByTagName("small")[0].innerHTML = `Analysing screen ${screenGlobalCounter}/${screensGlobal.length * timestamps.length} (screen batch ${b + 1}/${screensBatches.length}, timestamp ${ti + 1}/${timestamps.length})`;
+
+                await loadScreen(shadeMap, screens[i], zoom, worldSize);
+                await new Promise((res, rej) => {
+                    setTimeout(res, 1000);
+                });
+                const { data } = shadeMap.toGeoTiff();
+                const [x, y] = screens[i];
+                console.log("> copy x,y", x, y);
+                console.log("> copy x,y fixed", x - xMin - (((xMax - xMin) / batchCols) * batch['colStart']), y - yMin - (((yMax - yMin) / batchRows) * batch['rowStart']));
+                console.log("> area widthxheight", (width / cols) * batchCols, (height / rows) * batchRows)
+                copy(
+                    data, bitmap, // Data parameters
+                    innerWidth, innerHeight, // Screen width and height (px)
+                    x - xMin, y - yMin, // Coordinates withint bitmap (width batchCols*batchRows cells)
+                    (width / cols) * batchCols, (height / rows) * batchRows // Area width and height (px)
+                );
+            }
+            console.log("FINISHED batch", b, "downloading...");
+            await download(
+                bitmap,
+                nw, se, width, height, cols, rows,
+                batchCols, batchRows, batch['colStart'], batch['rowStart'], batch['colEnd'], batch['rowEnd'],
+                timestamp,
+                screensBatches.length > 1 ? b + 1 : undefined,
+            );
+            console.log("FINISHED downloading!");
+        }
+    }
+
+
+    // Display success 
+    loader.style.display = `none`;
+    div_panel.classList.remove("hidden");
+    dialog_success.getElementsByTagName("p")[0].innerHTML = "Your TIFF export was generated successfully!";
+    if (timestamps.length > 1) {
+        dialog_success.getElementsByTagName("p")[0].innerHTML += `<br/><br/>The TIFF files name contain the timestamp selected to allow the distinction between them.`
+    }
+    if (screensBatches.length > 1) {
+        dialog_success.getElementsByTagName("p")[0].innerHTML += `<br/><br/>The wideness of the area selected exceeds the browser processing capabilities. Do overcome this, it was divided into ${screensBatches.length} smaller and complementar areas, each corresponding to one TIFF file downloaded.`
+    }
+
+    dialog_success.classList.remove("hidden");
+
+    shadeMap.remove();
+}
+
+
+// Script kick-off
+window.onload = function () {
+    // Add default values to form 
+    document.getElementById("nw-lat").value = 38.735517;
+    document.getElementById("nw-lon").value = -9.136205;
+    document.getElementById("se-lat").value = 38.726796;
+    document.getElementById("se-lon").value = -9.117215;
+    document.getElementById("dates").value = "2025-06-21";
+    document.getElementById("hours").value = "12:00";
+    document.getElementById("hour-start").value = "06:00";
+    document.getElementById("hour-end").value = "18:00";
+    document.getElementById("iterations").value = 32;
+    document.getElementById("zoom").value = 15;
+    document.getElementById("filename").value = "stitch";
+
+    if (localStorage.getItem("mapboxKey")) {
+        document.getElementById("key-mapbox").value = localStorage.getItem("mapboxKey");
+    }
+    if (localStorage.getItem("shademapKey")) {
+        document.getElementById("key-shademap").value = localStorage.getItem("shademapKey");
+    }
+
+    // Event binding
+    dialog_success.getElementsByClassName("modal-close")[0].onclick = function () { dialog_success.classList.add("hidden"); }
+    dialog_error.getElementsByClassName("modal-close")[0].onclick = function () { dialog_error.classList.add("hidden"); }
+    dialog_help.getElementsByClassName("modal-close")[0].onclick = function () { dialog_help.classList.add("hidden"); }
+    dialog_about.getElementsByClassName("modal-close")[0].onclick = function () { dialog_about.classList.add("hidden"); }
+    Array.from(document.getElementsByClassName("help-modal")).forEach(el => el.onclick = function () { dialog_help.classList.remove("hidden"); })
+    Array.from(document.getElementsByClassName("about-modal")).forEach(el => el.onclick = function () { dialog_about.classList.remove("hidden"); })
+
+    document.getElementById("key-validation").onclick = function () {
+        let mapboxKey = document.getElementById("key-mapbox").value.trim();
+        let shademapKey = document.getElementById("key-shademap").value.trim();
+
+        let saveCredentials = document.getElementsByName("save-keys")[0];
+        if (saveCredentials.checked) {
+            localStorage.setItem('mapboxKey', mapboxKey);
+            localStorage.setItem("shademapKey", shademapKey);
+        } else {
+            localStorage.removeItem('mapboxKey');
+            localStorage.removeItem("shademapKey");
+        }
+
+
+        console.log("keys", mapboxKey, shademapKey);
+
+        if (!mapboxKey || !shademapKey) {
+            let message = `Invalid or missing keys!`;
+            dialog_error.getElementsByTagName("p")[0].innerHTML = message;
+            dialog_error.classList.remove("hidden");
+            throw new Error(message);
+        }
+
+        // If valid, start map and show form modal
+        dialog_keys.classList.add("hidden");
+        div_panel.classList.remove("hidden");
+
+        // Initialize map
+        mapboxgl.accessToken = mapboxKey;
+        var map = window.map = new mapboxgl.Map({
+            container: 'map',
+            zoom: 15,
+            center: {
+                lat: 38.735517, lng: -9.136205
+            },
+            style: 'mapbox://styles/mapbox/streets-v11',
+            hash: true
+        });
+
+        map.on('load', async () => {
+
+            // Once map has loaded, show form and enable download button
+            div_panel.getElementsByTagName("header")[0].getElementsByTagName("small")[1].innerHTML = "Adjust the parameters and choose the layer you want to export before downloading.";
+            document.getElementById("form").style = "";
+            document.getElementById("form").classList.remove("hidden");
+
+            const download_btn = document.getElementById("download");
+            download_btn.disabled = false;
+            download_btn.innerHTML = "Download shadows";
+            download_btn.onclick = function () { generateExport(map, shademapKey) };
+
+            // Listeners for layer form change, which should update shademap
+            const layer_shadow = document.getElementById(LAYER_SHADOW);
+            const layer_sun = document.getElementById(LAYER_SUN);
+
+            const observer = new MutationObserver(async (mutationsList) => { // Create a MutationObserver
+                mutationsList.forEach(async (mutation) => {
+
+                    // Only react when open chages to true (to avoid duplicate, as when one turns true, the other goes false)
+                    if (mutation.attributeName === "open" && mutation.target.hasAttribute("open")) {
+                        if (mutation.target.id == LAYER_SHADOW) {
+                            CURRENT_LAYER = LAYER_SHADOW;
+                            download_btn.innerHTML = "Download shadows";
+                            download_btn.disabled = false;
+                        } else if (mutation.target.id == LAYER_SUN) {
+                            CURRENT_LAYER = LAYER_SUN;
+                            download_btn.innerHTML = "Download sun exposure";
+                            download_btn.disabled = false;
+                        }
+                    }
+                });
+            });
+            observer.observe(layer_shadow, { attributes: true }); // Configure and start observing
+            observer.observe(layer_sun, { attributes: true }); // Configure and start observing
+        });
+    }
+}
---
+++ js/utils.js
@@ -0,0 +1,179 @@
+// Location and screen size
+const lng2pixel = (lng, zoom) => {
+    return ((lng + 180) / 360 * Math.pow(2, zoom)) * TILE_SIZE;
+}
+
+const lat2pixel = (lat, zoom) => {
+    return ((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom)) * TILE_SIZE
+}
+
+const unproject = (coords, zoom) => {
+    const [lat, lng] = coords;
+    return [Math.floor(lng2pixel(lng, zoom)), Math.floor(lat2pixel(lat, zoom))];
+}
+
+function coordinatesDifference(a, b) {
+    if (a > 0 && b > 0) return Math.abs(a - b);
+    if (a < 0 && b < 0) return Math.abs(Math.abs(a) - Math.abs(b));
+    if (a == 0) return Math.abs(b);
+    if (b == 0) return Math.abs(a);
+    return Math.abs(a) + Math.abs(b)
+}
+
+function roundToScreenSize(innerWidth, innerHeight, nw, se, worldSize) {
+
+    const nwM = mapboxgl.MercatorCoordinate.fromLngLat({ lng: nw[1], lat: nw[0] });
+    const seM = mapboxgl.MercatorCoordinate.fromLngLat({ lng: se[1], lat: se[0] });
+    const [xMin, yMin] = [nwM.x * worldSize, nwM.y * worldSize];
+    const [xMax, yMax] = [seM.x * worldSize, seM.y * worldSize];
+
+    const wSpan = xMax - xMin;
+    const hSpan = yMax - yMin;
+
+    const wScreens = Math.ceil(wSpan / innerWidth);
+    const hScreens = Math.ceil(hSpan / innerHeight);
+
+    const seMRounded = new mapboxgl.MercatorCoordinate(
+        (xMin + wScreens * innerWidth) / worldSize,
+        (yMin + hScreens * innerHeight) / worldSize,
+        0
+    );
+
+    const newSE = seMRounded.toLngLat();
+
+    const width = wScreens * innerWidth;
+    const height = hScreens * innerHeight;
+
+    return { width, height, wScreens, hScreens, newSE }
+
+}
+
+async function copy(source, destination, sw, sh, dx, dy, dw, dh) {
+    for (let i = 0; i < sh; i++) {
+        for (let j = 0; j < sw; j++) {
+            destination[(dy + i) * dw + dx + j] = source[i * sw + j];
+        }
+    }
+}
+
+
+// Map manipulation
+const shadeMapLoaded = (shadeMap) => {
+    return new Promise((res, rej) => {
+        shadeMap.on('idle', res);
+    });
+};
+
+const shadeMapBuild = (map, shademapKey, layer_type) => {
+    return new ShadeMap({
+        apiKey: shademapKey,
+        date: new Date(Date.UTC(2025, 6, 21, 12, 0)), // Temporary, will be changed before download
+        color: '#ff0000',
+        opacity: 1,
+        terrainSource: {
+            maxZoom: 25,
+            tileSize: 256,
+            getSourceUrl: ({ x, y, z }) => `https://s3.amazonaws.com/elevation-tiles-prod/terrarium/${z}/${x}/${y}.png`,
+            getElevation: ({ r, g, b, a }) => (r * 256 + g + b / 256) - 32768,
+            _overzoom: 18,
+        },
+        sunExposure: layer_type === LAYER_SUN ? {
+            enabled: true,
+            startDate: new Date(Date.UTC(2025, 6, 21, 8, 0)),
+            endDate: new Date(Date.UTC(2025, 6, 21, 18, 0)),
+            iterations: 32
+        } : { enabled: false },
+        getFeatures: async () => {
+            await SMUtils.mapLoaded(map);
+            const buildingData = map.querySourceFeatures('composite', { sourceLayer: 'building' }).filter((feature) => {
+                return feature.properties && feature.properties.underground !== "true" && (feature.properties.height || feature.properties.render_height)
+            });
+            return buildingData;
+        },
+    })
+}
+
+async function loadScreen(shadeMap, coords, zoom, worldSize) {
+    const [x, y] = coords;
+    map.setZoom(zoom);
+    const mercX = (x + innerWidth / 2) / worldSize;
+    const mercY = (y + innerHeight / 2) / worldSize;
+    const merc = new mapboxgl.MercatorCoordinate(mercX, mercY, 0);
+    const center = merc.toLngLat();
+    map.setCenter(center);
+    const smLoaded = shadeMapLoaded(shadeMap)
+    const mapboxLoaded = SMUtils.mapLoaded(map);
+    await Promise.all([mapboxLoaded, smLoaded]);
+    return;
+}
+
+
+// TIFF file generation
+async function download(
+    data, // Exported data to download
+    nw, se, width, height, cols, rows, // Area global values
+    batchCols, batchRows, colStart, rowStart, colEnd, rowEnd, // Sheet batch information
+    timestamp, // Timestamp is added to file name (list or instance)
+    subarea // Subarea number for file name purposes
+) {
+
+    console.log("start", colStart, rowStart);
+    console.log("end", colEnd, rowEnd);
+    console.log("cols", cols, "batchcols", batchCols);
+    console.log("rows", rows, "batchrows", batchRows);
+
+    let latitudePerRow = coordinatesDifference(nw[0], se[0]) / rows;
+    let latitudeStart = nw[0] - rowStart * latitudePerRow; // We start on top nw, so we go south (-)
+
+    let longitudePerColumn = coordinatesDifference(se[1], nw[1]) / cols;
+    console.log("longitudePerColumn", longitudePerColumn);
+    let longitudeStart = nw[1] + colStart * longitudePerColumn; // We start on top nw, so we go east (+)
+
+    const ModelTiepoint = [0, 0, 0, longitudeStart, latitudeStart, 0];
+    const ModelPixelScale = [coordinatesDifference(se[1], nw[1]) / width, coordinatesDifference(nw[0], se[0]) / height, 0];
+
+    console.log("ModelTiepoint", ModelTiepoint);
+    console.log("ModelPixelScale", ModelPixelScale);
+
+    const metadata = {
+        width: (width / cols) * batchCols,
+        height: (height / rows) * batchRows,
+        ModelTiepoint,
+        ModelPixelScale,
+        GeographicTypeGeoKey: 4326,
+        // GDAL_NODATA,
+        GeogCitationGeoKey: 'WGS 84',
+    };
+    console.log(data.length);
+    console.log("Starting GeoTIFF.writeArrayBuffer...", metadata);
+    const arrayBuffer = await GeoTIFF.writeArrayBuffer(
+        Array.from(data),
+        metadata
+    );
+    console.log("GeoTIFF.writeArrayBuffer finished.");
+    const typedArray = new Uint8Array(arrayBuffer);
+    const charCode = typedArray.reduce((all, char) => {
+        return (all += String.fromCharCode(char));
+    }, "");
+    const base64 = btoa(charCode);
+    const datauri = `data:image/tiff;base64,${base64}`;
+
+    const link = document.createElement("a");
+    link.href = datauri;
+
+    let filename = document.getElementById("filename").value;
+    if (!filename) filename = "stitch";
+    let append = Array.isArray(timestamp) ? `_${getDateString(timestamp[0])}_${getDateString(timestamp[1])}` : `_${getDateString(timestamp)}`;
+    if (subarea) append = `_part${subarea}`
+    link.download = `${filename}${append}.tiff`
+
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+
+// Other useful methods
+const pad = (num) => String(num).padStart(2, '0');
+const getDateString = (timestamp) => `${timestamp.getFullYear()}${pad(timestamp.getMonth() + 1)}${pad(timestamp.getDate())}_${pad(timestamp.getHours())}${pad(timestamp.getMinutes())}${pad(timestamp.getSeconds())}`
